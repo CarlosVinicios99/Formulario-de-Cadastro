248799ac9065f72639c62369c1c9f7796d6bded7
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-11-18 21:10:40 -0300

    config: configuração inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Form from './components/Form'
+
+function App() {
+
+
+  return (
+   <div>
+    <h1 className="font-bold text-3xl">Cadastro</h1>
+    <Form/>
+   </div>
+  )
+}
+
+export default App

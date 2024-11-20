4babe8f0fdef568b6ccd2c7ce934de6a14877171
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-11-20 15:32:27 -0300

    feature: criação de validação de formulário

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,27 @@
+import { useForms } from "../hooks/useForms"
+import { FormSchema } from "../schemas/formSchema"
+
 const Form = () => {
-  return (
-    <div>Form</div>
-  )
+
+  const {register, handleSubmit, errors} = useForms();
+
+  const onSubmit = (data: FormSchema) => {
+    console.log(data)
+  }
+
+  return  <form onSubmit={handleSubmit(onSubmit)}>
+    <div>
+      <label htmlFor="name">Nome: </label>
+      <input 
+        type="text" 
+        id="name" 
+        placeholder="Digite o seu nome" 
+        {...register("name")}
+      />
+      {errors.name && (<small>{errors.name.message}</small>)}
+    </div>
+    <button type="submit">Cadastrar</button>
+  </form>
 }
 
 export default Form
